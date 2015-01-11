d59253186d5de23506b5d0c4515a77757363f8a6
Volk204 <volk204@mail.ru>
2015-01-11 22:44:40 +0100

    wave: switch to phone-hdpi-dalvik-heap.mk

--- device.mk
+++ device.mk
@@ -201,7 +201,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
 
-include frameworks/native/build/phone-hdpi-512-dalvik-heap.mk
+include frameworks/native/build/phone-hdpi-dalvik-heap.mk
 
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
