699a9daa2156728915ca853388567d0dd93325e3
Alberto96 <albertoguanti@gmail.com>
2015-08-02 17:46:39 +0200

    wave: remove obsolete features

--- device.mk
+++ device.mk
@@ -83,8 +83,7 @@ PRODUCT_PACKAGES := \
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
-	setup_fs \
-	bml_over_mtd
+	setup_fs
 
 # Usb accessory
 PRODUCT_PACKAGES += \
@@ -149,28 +148,30 @@ PRODUCT_COPY_FILES += \
 # The OpenGL ES API level that is natively supported by this device.
 # This is a 16.16 fixed point number
 PRODUCT_PROPERTY_OVERRIDES := \
-    ro.opengles.version=131072
+	ro.opengles.version=131072 \
+	debug.hwui.render_dirty_regions=false \
+	ro.zygote.disable_gl_preload=true
 
 # These are the hardware-specific settings that are stored in system properties.
 # Note that the only such settings should be the ones that are too low-level to
 # be reachable from resources or other mechanisms.
 PRODUCT_PROPERTY_OVERRIDES += \
-       wifi.interface=wlan0 \
-       mobiledata.interfaces=pdp0,eth0,gprs,ppp0 \
-       rild.libpath=/system/lib/libmocha-ril.so \
-       ro.sf.lcd_density=240 \
-       ro.bq.gpu_to_cpu_unsupported=1 \
-       ro.config.low_ram=true \
+	wifi.interface=wlan0 \
+	mobiledata.interfaces=pdp0,eth0,gprs,ppp0 \
+	rild.libpath=/system/lib/libmocha-ril.so \
+	ro.sf.lcd_density=240 \
+	ro.bq.gpu_to_cpu_unsupported=1 \
+	ro.config.low_ram=true \
 
 # SGX540 is slower with the scissor optimization enabled
 PRODUCT_PROPERTY_OVERRIDES += \
-       ro.hwui.disable_scissor_opt=true
+	ro.hwui.disable_scissor_opt=true
 
 # enable Google-specific location features,
 # like NetworkLocationProvider and LocationCollector
 PRODUCT_PROPERTY_OVERRIDES += \
-        ro.com.google.locationfeatures=1 \
-        ro.com.google.networklocation=1
+	ro.com.google.locationfeatures=1 \
+	ro.com.google.networklocation=1
 
 # Extended JNI checks
 # The extended JNI checks will cause the system to run more slowly, but they can spot a variety of nasty bugs 
