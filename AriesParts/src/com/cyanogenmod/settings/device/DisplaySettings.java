e742cca1b8166706f06c91f5e0c03111d2f0d190
Alberto96 <albertoguanti@gmail.com>
2015-08-11 13:53:47 +0200

    wave: AriesParts: remove TvOut support
    fixes Advanced Display crash

--- AriesParts/src/com/cyanogenmod/settings/device/DisplaySettings.java
+++ AriesParts/src/com/cyanogenmod/settings/device/DisplaySettings.java
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.hardware.TvOut;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
